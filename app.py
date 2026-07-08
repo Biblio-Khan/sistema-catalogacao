f26001ccd21c0cc637ad28fc8837fd39a54f2c99
Biblio-Khan <bibliokhancontato@gmail.com>
2026-07-08 10:26:38 -0300

    Refactor examiner section and add co-supervisor inputs

--- app.py
+++ app.py
@@ -20,22 +20,37 @@ with st.form("form_cadastro"):
     area_concentracao = st.radio("Área de concentração", ("Ciência e Tecnologia das Radiações e Reatores Nucleares", "Ciência e Tecnologia dos Minerais e Meio Ambiente", "Ciência e Tecnologia dos Materiais"))
     ano_defesa = st.text_input("Ano da defesa", placeholder="Ex: 2026")
     
-    # --- Seção: Banca Examinadora com Titulação ---
+   # --- Seção: Banca Examinadora ---
     st.subheader("Dados da Banca Examinadora")
-    num_orientadores = st.selectbox("Quantos orientadores tem no seu trabalho?", options=[1, 2, 3])
     
+    # Orientadores
+    st.write("### Orientador(es)")
+    num_orientadores = st.selectbox("Quantos orientadores tem no seu trabalho?", options=[1, 2, 3], key="num_o")
     lista_orientadores = []
     titulos_opcoes = ["Dr.", "Dra.", "Me.", "Ma."]
     
     for i in range(num_orientadores):
         col1, col2 = st.columns([3, 1])
         with col1:
-            nome = st.text_input(f"Nome do orientador {i+1}", key=f"nome_{i}")
+            nome = st.text_input(f"Nome do orientador {i+1}", key=f"nome_o_{i}")
         with col2:
-            titulacao = st.selectbox(f"Título", titulos_opcoes, key=f"tit_{i}")
-        
+            titulacao = st.selectbox(f"Título O", titulos_opcoes, key=f"tit_o_{i}")
         if nome:
             lista_orientadores.append(f"{titulacao} {nome}")
+
+    # Coorientadores
+    st.write("### Coorientador(es)")
+    num_coorientadores = st.selectbox("Quantos coorientadores tem no seu trabalho?", options=[0, 1, 2, 3], key="num_c")
+    lista_coorientadores = []
+    
+    for i in range(num_coorientadores):
+        col1, col2 = st.columns([3, 1])
+        with col1:
+            nome = st.text_input(f"Nome do coorientador {i+1}", key=f"nome_c_{i}")
+        with col2:
+            titulacao = st.selectbox(f"Título C", titulos_opcoes, key=f"tit_c_{i}")
+        if nome:
+            lista_coorientadores.append(f"{titulacao} {nome}")
     # -----------------------------------------------
 
     num_folhas = st.number_input("Número total de folhas", min_value=1, step=1)
